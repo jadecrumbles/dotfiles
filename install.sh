4f6d466de514b7ee9b2479b5aa17825e037e075a
Tetra <tetra.silv@gmail.com>
2025-10-19 17:26:04 -0400

    More stuff.

--- install.sh
+++ install.sh
@@ -1,19 +1,70 @@
 GIT_ROOT=$(git rev-parse --show-toplevel)
 
+# ZSH & ZShRC
+echo "[zsh] Installing..."
+sudo pacman -S --needed zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 if [ -L "~/.zshrc" ]; then
+    echo "[zshrc] Removing existing..."
     rm "~/.zshrc"
-    echo "asdf"
 fi
+echo "[zshrc] Linking..."
 ln -sf "$GIT_ROOT/src/.zshrc" ~/.zshrc
 
-if [ -L "~/.zshrc" ]; then
-    rm "~/.zshrc"
-    echo "asdf"
+# BashRC
+if [ -L "~/.bashrc" ]; then
+    echo "[bashrc] Removing existing..."
+    rm "~/.bashrc"
 fi
+echo "[bashrc] Linking..."
 ln -sf "$GIT_ROOT/src/.bashrc" ~/.bashrc
 
+# Code (VSCode-OSS)
+echo "[code] Installing..."
+sudo pacman -S --needed code
 if [ -L "~/.vscode-oss" ]; then
+    echo "[code] Removing existing..."
     rm -r "~/.vscode-oss"
-    echo "asdf"
 fi
+echo "[code] Linking..."
 ln -sf "$GIT_ROOT/src/.vscode-oss" ~/.vscode-oss
+
+# Cinnamon (Base)
+echo "[cinnamon] Installing..."
+sudo pacman -S --needed cinnamon
+
+# Cinnamon (icons)
+echo "[cinnamon.icons] Installing..."
+if [ -L "~/.icons" ]; then
+    echo "[cinnamon.icons] Removing existing..."
+    rm -r "~/.icons"
+fi
+echo "[cinnamon.icons] Linking..."
+ln -sf "$GIT_ROOT/src/.icons" ~/.icons
+
+# Cinnamon (themes)
+echo "[cinnamon.themes] Installing..."
+if [ -L "~/.themes" ]; then
+    echo "[cinnamon.themes] Removing existing..."
+    rm -r "~/.themes"
+fi
+echo "[cinnamon.themes] Linking..."
+ln -sf "$GIT_ROOT/src/.icons" ~/.icons
+
+# XProfile
+echo "[xprofile] Installing..."
+if [ -L "~/.xprofile" ]; then
+    echo "[xprofile] Removing existing..."
+    rm "~/.xprofile"
+fi
+echo "[xprofile] Linking..."
+ln -sf "$GIT_ROOT/src/.xprofile" ~/.xprofile
+
+# Alacritty
+echo "[alacritty] Installing..."
+if [ -L "~/.config/alacritty" ]; then
+    echo "[alacritty] Removing existing..."
+    rm -r "~/.config/alacritty"
+fi
+echo "[alacritty] Linking..."
+ln -sf "$GIT_ROOT/src/alacritty" ~/.config/alacritty
